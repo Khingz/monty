cc12b1ccda7dc12a7a427d138345880697a9496f
Khingz <kingsley995mr@gmail.com>
2023-06-20 08:21:27 -0700

    pall implemetation with segmwntation fault

--- opcodes_func1.c
+++ opcodes_func1.c
@@ -39,7 +39,15 @@ void push(stack_t **stack, unsigned int line_number)
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-        (void)line_number;
-	printf("from pall\n");
+	stack_t *temp = (*stack)->next;
+
+	if (!stack || *stack == NULL)
+		return;
+	(void)line_number;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	(void)line_number;
 }
