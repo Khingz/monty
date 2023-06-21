cba0cc050f0a5016c1942961774183d5413b0788
Khingz <kingsley995mr@gmail.com>
2023-06-21 12:14:14 -0700

    refactored execute function

--- bytecode/00.m
+++ bytecode/00.m
@@ -3,4 +3,4 @@ push 3
 
 push 1
 push 2
-pall
+pall 2
--- execute.c
+++ execute.c
@@ -10,46 +10,43 @@ int execute(FILE *fd)
 {
 	char *line = NULL;
 	unsigned int line_num = 0;
-	int read, token_count = 0;
+	int token_count = 0;
 	size_t len = 0;
 	char *delims = " \n\t\a\b";
 	void (*opcode_f)(stack_t **stack, unsigned int line_number);
 	stack_t *stack = NULL;
 
-	head_node(&stack);
-	while (1)
+	if (head_node(&stack) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	while (getline(&line, &len, fd) != -1)
 	{
 		line_num++;
-		line = NULL;
-		read = getline(&line, &len, fd);
-		if (read == -1)
-		{
-			free(line);
-			if (line == NULL)
-				break;
-			return (EXIT_FAILURE);
-		}
-		if (line[strlen(line) - 1] == '\n')
-			line[strlen(line) - 1] = '\0';
 		tokens = tokenize(line, delims, &token_count);
 		if (tokens == NULL)
 		{
-			fprintf(stderr, "token error %d\n", line_num);
+			fprintf(stderr, "Error: malloc failed\n");
+			free_stack(&stack);
 			return (EXIT_FAILURE);
 		}
 		opcode_f = get_opcode_func(tokens[0]);
 		if (opcode_f == NULL)
 		{
-			if (line == NULL || line[0] == '\0' || empty_line(line, delims))
+			if (empty_line(line, delims))
                                 continue;
-			free(line);
-			fprintf(stderr, "L<%u>: unknown instruction <%s>\n", line_num, tokens[0]);
-			return (EXIT_FAILURE);
+			free_stack(&stack);
+			free_tokens(tokens, token_count);
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_num, tokens[0]);
+			break;
 		}
 		opcode_f(&stack, line_num);
-		free(line);
 		free_tokens(tokens, token_count);
 	}
 	free_stack(&stack);
+	if (line && *line == 0)
+	{
+		free(line);
+		fprintf(stderr, "Error: malloc failed\n");
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
