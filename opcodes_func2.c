6314443f27363e72b8d177aa07c2a5669a3f50b8
Khingz <kingsley995mr@gmail.com>
2023-06-22 04:03:52 -0700

    mod function pmplemetation

--- opcodes_func2.c
+++ opcodes_func2.c
@@ -58,4 +58,41 @@ void div(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 }
 
+/**
+ * mul - Multiplies top 2 vsl of the list
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		token_error(short_stack_error(line_number, "mul"));
+		return;
+	}
 
+	(*stack)->next->next->n *= (*stack)->next->n;
+	pop(stack, line_number);
+}
+
+/* mod - Computes modulus
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		token_error(short_stack_error(line_number, "mod"));
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		token_error(div_error(line_number));
+		return;
+	}
+
+	(*stack)->next->next->n %= (*stack)->next->n;
+	pop(stack, line_number);
+}
